26a2f142b785d830e1994c5a424b47c1420907e3
KarenNgala <ngalalalu@gmail.com>
2020-08-03 12:01:25 +0300

    Implement search

--- picha/urls.py
+++ picha/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$', views.homepage, name='landing'),
-    url('^gallery/', views.gallery, name='gallery')
+    url('^gallery/', views.gallery, name='gallery'),
+    url('^search/', views.search, name='search'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
